aebc63b4d57a2eddf40758ac093e2695bdd0f9d5
Samantha <sam@thriveweb.com.au>
2018-07-17 07:19:48 +1000

    insert hero img

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+import './Hero.css'
+
 const Hero = (props) => (
 
   <div>
--- src/components/Opener.js
+++ src/components/Opener.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+import './Opener.css'
+
 const Opener = () => (
 
   <div>

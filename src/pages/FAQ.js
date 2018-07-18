e91aff55d94890eb9309c5013031b1c9fc9d52eb
Sam <sam@thriveweb.com.au>
2018-07-18 15:32:17 +1000

    adding content

--- src/pages/FAQ.js
+++ src/pages/FAQ.js
@@ -32,17 +32,19 @@ const FAQPage = () => (
       <div className="small content">
         <LeftColumn
           Content={
-            <p>
-              Proin eget ipsum quis lectus molestie rutrum eu in sem. Praesent tincidunt neque nunc, commodo pharetra odio pellentesque aliquam. Pellentesque tincidunt diam dolor, id dignissim arcu varius sit amet. Proin vel gravida neque, nec interdum dolor.
-            </p>
+            <div>
+              <p>Robert accepts briefs to appear in courts all Australian states and territories. He accepts briefs both paper and electronic format. Electronic briefs may be delivered either by email or on USB or other media via his PO Box. PDF format is preferred. Paper briefs are to be addressed to his Post Office box. Please allow appropriate time for delivery of all hard-copy mail communications.</p>
+              <p>Robert happily confers with clients in the offices of their solicitor and when he works interstate does so on a fly-in-fly-out basis. He requires all airfares and accommodation bookings are to be made and paid for by his instructing solicitors. Robert only accepts briefs for clients via their solicitor.</p>
+            </div>
           }
         />
 
         <RightColumn
           Content={
-            <p>
-              Donec porta, ex sed ullamcorper vehicula, augue diam cursus orci, porta pretium mauris leo iaculis metus. Mauris ac elit sit amet orci dictum pellentesque. Praesent eget aliquam libero. Aenean tempor ligula sed diam gravida, placerat blandit magna bibendum.
-            </p>
+            <div>
+              <p>Robert only accepts briefs for clients via their solicitor. He does not accept briefs direct from the public. This is because pre-trial litigation work is complex, labour intensive, and requires the the attention of an experienced solicitor. No work will be commenced by Robert (unless urgent) until appropriate agreement is reached about the terms of his retainer. Fee estimates are available upon request. </p>
+              <p>It is often necessary to review the brief before any fee estimate can be made. In appropriate cases Robert is willing work on a deferred or conditional basis. In large, complex or urgent matters he may require some payment for perusal of the brief before considering alternate fee arrangements. Before sending a brief you should phone Robert to discuss the matter and confirm his availability.</p>
+            </div>
           }
         />
 
@@ -52,27 +54,41 @@ const FAQPage = () => (
           accordionFields={[
             {
               title: "What is a barrister?",
-              content: "Donec porta, ex sed ullamcorper vehicula, augue diam cursus orci, porta pretium mauris leo iaculis metus. Mauris ac elit sit amet orci dictum pellentesque. Praesent eget aliquam libero. Aenean tempor ligula sed diam gravida, placerat blandit magna bibendum."
+              content: <div>
+              <p>It is often necessary to review the brief before any fee estimate can be made. In appropriate cases Robert is willing work on a deferred or conditional basis. In large, complex or urgent matters he may require some payment for perusal of the brief before considering alternate fee arrangements. Before sending a brief you should phone Robert to discuss the matter and confirm his availability.</p>
+            </div>
             },
 
             {
               title: "How does a barrister differ from a solicitor?",
-              content: "Donec porta, ex sed ullamcorper vehicula, augue diam cursus orci, porta pretium mauris leo iaculis metus. Mauris ac elit sit amet orci dictum pellentesque. Praesent eget aliquam libero. Aenean tempor ligula sed diam gravida, placerat blandit magna bibendum."
+              content: <div>
+              <p>Solicitors and barristers are both legal professionals who are permitted to appear in courts. Most solicitors spend their careers performing transactional work for clients, such as property transfers, drawing wills, preparing contracts, etc.</p>
+              <p>Litigation solicitors mostly prepare cases and then brief a barrister to appear at court on their client's behalf. Solicitors also brief barristers to advise their clients on specialist areas of legal knowledge. Solicitors often work in law firms and may choose to only represent special categories of client. </p>
+              <p>In contrast, barristers are required to work only as sole traders and are ethically bound (subject to agreement being reached as to their fees) to accept briefs on behalf of any client.</p>
+            </div>
             },
 
             {
               title: "How do barristers charge?",
-              content: "Donec porta, ex sed ullamcorper vehicula, augue diam cursus orci, porta pretium mauris leo iaculis metus. Mauris ac elit sit amet orci dictum pellentesque. Praesent eget aliquam libero. Aenean tempor ligula sed diam gravida, placerat blandit magna bibendum."
+              content: <div>
+              <p>Barristers fees will vary depending on the nature of the work required, their special expertise, and their seniority. Barristers charge fees that are either based on the time spent or, on occasion, a fixed fee on brief.</p>
+              <p>Barristers are required to disclose their fees before commencing work. If urgent work is required before a fee disclosure can be made, then the barrister must make a disclosure at the earliest opportunity. A barrister is (usually) not to perform any work for a client unless a cost agreement has first been reached.</p>
+              <p>Barristers are sometimes willing to work on a deferred payment basis. Other barristers may be willing to work on a conditional basis - that is - where payment depends wholly or partly upon success in the case.</p>
+              </div>
             },
 
             {
               title: "Do I also need a solicitor?",
-              content: "Donec porta, ex sed ullamcorper vehicula, augue diam cursus orci, porta pretium mauris leo iaculis metus. Mauris ac elit sit amet orci dictum pellentesque. Praesent eget aliquam libero. Aenean tempor ligula sed diam gravida, placerat blandit magna bibendum."
+              content: <div>
+              <p>Some barristers are willing to accept direct briefs from the public. This usually occurs with barristers conducting criminal work in lower courts. Many (probably most) barristers will only accept client briefs via a solicitor's firm. Litigation work is often very complex and labour intensive. Barristers must work as sole traders so they lack the human and other resources that a solicitors firm can devote to pre-trial paperwork. Robert only accepts briefs via a solicitor.</p>
+              </div>
             },
 
             {
               title: "How do I employ a barrister?",
-              content: "Donec porta, ex sed ullamcorper vehicula, augue diam cursus orci, porta pretium mauris leo iaculis metus. Mauris ac elit sit amet orci dictum pellentesque. Praesent eget aliquam libero. Aenean tempor ligula sed diam gravida, placerat blandit magna bibendum."
+              content: <div>
+              <p>If you are a member of the public and wish to directly engage a barrister then you should phone around to find someone who is suitable. If you already have a solicitor then he or she will advise you about which barristers are best suited to your circumstances. You can carry out your own research into the barristers suggested, but your solicitor is often the best source of information in this regard.</p>
+            </div>
             },
           ]}
 

e45075e4cc82f860a53a2c1f7e41b48d22608270
Eric Jinks <ericjinks@gmail.com>
2018-07-20 10:49:14 +1000

    Config init

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,5 @@ module.exports = {
   siteMetadata: {
     title: 'Davis Legal',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: ['gatsby-plugin-react-helmet', `gatsby-plugin-netlify-cms`],
 }

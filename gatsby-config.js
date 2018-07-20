a5360ca3f4408666461f4da10d7069a33f358c92
Eric Jinks <ericjinks@gmail.com>
2018-07-20 14:07:58 +1000

    Update gatsbyconfig

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,48 @@ module.exports = {
   siteMetadata: {
     title: 'Davis Legal',
   },
-  plugins: ['gatsby-plugin-react-helmet', `gatsby-plugin-netlify-cms`],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-transformer-yaml',
+
+    // Add static assets before markdown files
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/static/img`,
+        name: 'images',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/content`,
+        name: 'pages',
+      },
+    },
+
+    // images
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          // gatsby-remark-relative-images must
+          // go before gatsby-remark-images
+          'gatsby-remark-relative-images',
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 800,
+              linkImagesToOriginal: false,
+            },
+          },
+        ],
+      },
+    },
+
+    `gatsby-plugin-netlify-cms`,
+  ],
 }
